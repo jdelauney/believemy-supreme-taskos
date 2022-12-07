52f50bd2dcdd85fb5990443565c17839a51e0020
jdelauney <delauney.jerome@gmail.com>
2022-12-07 13:28:30 +0100

    revert

---
+++ scripts/app.js
@@ -0,0 +1,19 @@
+import { createElement } from "./helpers/domUtils.js";
+import { fetchJSON } from "./helpers/fetchUtils.js";
+import { TaskList } from "./taskApp/Components/TaskList.js";
+
+try {
+  const data = await fetchJSON('https://dummyjson.com/todos?skip=0&limit=20')
+  const tasks = data.todos
+  console.log(tasks)
+  const taskList = new TaskList(tasks)
+  taskList.displayInto(document.getElementById('tasks'))
+} catch (err) {
+  const notificationError = createElement('div', 'Chargement des tâches impossible !', {
+    class: 'notification notification--danger',
+    role: 'alert'
+  })
+  document.body.prepend(notificationError)
+  console.error(err)
+}
+
