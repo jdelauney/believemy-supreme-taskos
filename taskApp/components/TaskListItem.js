ca1d32b9a0b4a87bb83c86a394cb9c97516cbb3c
jdelauney <delauney.jerome@gmail.com>
2022-12-13 23:03:07 +0100

    feat(editTask): edition d'une tache

--- taskApp/components/TaskListItem.js
+++ taskApp/components/TaskListItem.js
@@ -11,20 +11,24 @@ export class TaskListItem {
   /* @type {HTMLElement} */
   #element
   #removeCallback
+  #editCallback
+  #taskTitleElement
   _task   
   /**
    * 
    * @param {Task} task
    * @param {function(TaskListItem):void} removeCallback
+   * @param {function(TaskListItem):void} editCallback
    */
-  constructor(task, removeCallback) {
+  constructor(task, removeCallback, editCallback) {
     
     console.log('item => ', task)
     this.#removeCallback = removeCallback
+    this.#editCallback = editCallback
 
     this._task = {
       id: task.id,
-      title: task.todo,
+      todo: task.todo,
       completed : task.completed
     }
     const id = 'task-'+ task.id
@@ -39,6 +43,7 @@ export class TaskListItem {
     </span>
 
     <span>${task.todo}</span>`
+
     const dropdown = createElement('div', '', { class: 'task-app__task__menu' })
     dropdown.innerHTML = `<button class="btn btn--task-menu">
       <svg class="icon-svg">
@@ -46,6 +51,14 @@ export class TaskListItem {
       </svg>
     </button>
     <ul class="task-app__task__menu__actions">
+      <li>
+        <button data-task-edit class="btn btn--dropdown-action">
+          <svg class="icon-svg icon--primary">
+            <use href="#icon-edit"/>
+          </svg>
+          Editer
+        </button>
+      </li>
       <li>
         <button data-task-remove class="btn btn--dropdown-action">
           <svg class="icon-svg icon--danger">
@@ -60,10 +73,25 @@ export class TaskListItem {
       .querySelector('[data-task-remove]')
       .addEventListener("click", evt => this._removeHandler(evt))
     
+    dropdown
+    .querySelector('[data-task-edit]')
+      .addEventListener("click", evt => this._editHandler(evt))
+
+    this.#taskTitleElement = label.querySelector('span:last-child')
+    
     li.append(label)
     li.append(dropdown) 
   
     this.#element = li
+  }
+
+  /**
+   * @param {PointerEvent} evt
+   */
+  _editHandler(evt) {
+    if (this.#editCallback) {
+      this.#editCallback(this)
+    }
   }
 
   _removeHandler(evt) {
@@ -75,6 +103,18 @@ export class TaskListItem {
 
   get element() {
     return this.#element
+  }
+
+  get title() {
+    return this._task.todo
+  }
+
+  /**
+   * @param {string} newTitle
+   */
+  set title(newTitle) {
+    this._task.todo = newTitle
+    this.#taskTitleElement.innerText = newTitle
   }
 
 }
